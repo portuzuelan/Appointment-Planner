7bae724cf6e6ab21b44a09029b811e534cf9da89
Alex 🐵 <aomarpereira@Antonios-MacBook-Pro.local>
2018-03-06 14:54:32 -0500

    Initial Commit

---
+++ Appointment Planner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Appointment Planner
+//
+//  Created by Antonio Omar Pereira on 2/12/18.
+//  Copyright © 2018 Antonio Omar Pereira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
